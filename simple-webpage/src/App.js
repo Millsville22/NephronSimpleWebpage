72f25ecf141028c864d8a428427647df98b4d259
Millsville22 <milescolbert@gmail.com>
2022-04-07 22:21:15 -0400

    Login and Logout Functionality

--- simple-webpage/src/App.js
+++ simple-webpage/src/App.js
@@ -1,32 +1,49 @@
-import logo from './logo.svg';
-//import './App.css';
 import React, { useState } from 'react';
 import LoginPage from './component/pages/LoginPage'
 
 function App() {
+
+  // Credentials to login with
+  const adminUser = {
+    email: "nephron@pharma.com",
+    password: "nephron803"
+  }
 
   const [user, setUser] = useState({email: "", userName: "", password: ""});
   const [error, setError] = useState("");
 
   const Login = details => {
     console.log(details);
+
+    if (details.email === adminUser.email && details.password === adminUser.password) {
+      console.log("Logged in");
+      setUser({
+        email: details.email,
+        userName: details.userName
+      });
+    }
+    else {
+      console.log("Wrong email and/or password");
+      setError("Wrong email and/or password");
+    }
   }
 
   const Logout = () => {
     console.log("Logout");
+    setUser({ email: "", userName: "" });
   }
 
 
   return (
     <div className="App">
-      {(user.password != "") ? (
+      {(user.email !== "") ? (
         <div className="greeting">
           <h2>Greetings, <span>{user.userName}</span></h2>
-          <button>Logout</button>
+          <button onClick={Logout}>Logout</button>
         </div>
 
       ) : (
-        <LoginPage />
+        <LoginPage Login={Login} error={error}/>
       )}
 
   
--- simple-webpage/src/component/pages/LoginPage.js
+++ simple-webpage/src/component/pages/LoginPage.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function LoginPage() {
+function LoginPage({ Login, error }) {
+    const [details, setDetails] = useState({email: "", userName: "", password: ""});
+
+    const submitHandle = e => {
+        e.preventDefault();
+
+        Login(details);
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandle}>
             <div className="form-inner">
                 <h2>Login</h2>
-
+                {(error !== "") ? ( <div className="error">{error}</div> ) : "" }
                 <div className="form-group">
                     <label htmlFor="email">Email:</label>
-                    <input type="text" name="email" id="email"/>
+                    <input type="text" name="email" id="email" 
+                        onChange={e => setDetails({...details, email: e.target.value})} value={details.email} />
                 </div>
 
                 <div classname="form-group">
                     <label htmlFor="userName">Username:</label>
-                    <input type="text" name="userName" id="userName"/>
+                    <input type="text" name="userName" id="userName"
+                        onChange={e => setDetails({...details, userName: e.target.value})} value={details.userName} />
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="password">Password:</label>
-                    <input type="text" name="password" id="password"/>
+                    <input type="text" name="password" id="password"
+                        onChange={e => setDetails({...details, password: e.target.value})} value={details.password} />
                 </div>
+                <input type="submit" value="Login" />
 
             </div>
         </form>
